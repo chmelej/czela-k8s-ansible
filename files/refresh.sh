3f3aedb97ef0eacf14933bfb826b0c2611263266
Jan Chmelensky <jan.chmelensky@fcrmedia.com>
2019-10-30 22:21:59 +0100

    dashboard works

--- files/refresh.sh
+++ files/refresh.sh
@@ -1,2 +1,3 @@
 wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml -O kube-flannel.yml
-wget https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml -O kubernetes-dashboard.yaml
+#wget https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml -O kubernetes-dashboard.yaml
+wget https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended.yaml -O kubernetes-dashboard.yaml
