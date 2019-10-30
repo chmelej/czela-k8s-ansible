6af72b31bf97995b054b82b3b131eb793a97bec5
Jan Chmelensky <jan.chmelensky@fcrmedia.com>
2019-10-30 20:35:56 +0100

    1st commit

---
+++ files/refresh.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml -O kube-flannel.yml
+wget https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml -O kubernetes-dashboard.yaml
